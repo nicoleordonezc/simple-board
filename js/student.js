74e9a2527e0aba43ee86ab4d737003af679d71cf
Nicoleordonezc <nicoloe24@gmail.com>
2025-03-05 18:13:29 -0500

    feat: :lipstick: Started JS

--- js/student.js
+++ js/student.js
@@ -1,7 +1,13 @@
 
 const add__student = document.querySelector("#add__student");
-const dialog__add_student = document.querySelector("dialog__add_student");
+const dialog__add_student = document.querySelector("#dialog__add_student");
+const dialog__close = document.querySelector("#dialog_close");
 
-add__student.addEventListener("click", ()=> {
+
+add__student.addEventListener("click", ()=>{
     dialog__add_student.showModal();
-})
+});
+
+dialog__close.addEventListener("click", ()=>{
+    dialog__add_student.close();
+});
