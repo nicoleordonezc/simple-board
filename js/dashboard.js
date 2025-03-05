43fe787cb7c28615f58504fe41256be3bdbae3e9
Nicoleordonezc <nicoloe24@gmail.com>
2025-03-05 15:55:56 -0500

    feat: :lipstick: JS is complete for the main cards

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,13 +1,18 @@
 import { findAllStudent } from "./students.js";
 import {findAllCourse} from "./course.js"
 import {findAllPayments} from "./payments.js"
+import {findAllUsers} from "./users.js"
 
 const students = document.querySelector ("#students > strong");
 const course = document.querySelector("#course > strong");
-const payments = document.querySelector("#payments > strong")
+const payments = document.querySelector("#payments > strong");
+const users = document.querySelector("#users > strong");
 
-students.innerHTML = findAllStudent()
-course.innerHTML = findAllCourse()
+students.innerHTML = findAllStudent();
+course.innerHTML = findAllCourse();
+users.innerHTML = findAllUsers ();
+
 const money = JSON.parse(findAllPayments());
 payment.dataset.format = money.format;
-payments.innerHTML = findAllPayments()
+payments.innerHTML = findAllPayments();
+
