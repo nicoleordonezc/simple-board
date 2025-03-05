bd13820da1d44983bdd21526f44578cfd60dca47
Nicoleordonezc <nicoloe24@gmail.com>
2025-03-05 13:03:30 -0500

    feat: :construction: HTML in progress

---
+++ js/student.js
@@ -0,0 +1,7 @@
+
+const add__student = document.querySelector("#add__student");
+const dialog__add_student = document.querySelector("dialog__add_student");
+
+add__student.addEventListener("click", ()=> {
+    dialog__add_student.showModal();
+})
